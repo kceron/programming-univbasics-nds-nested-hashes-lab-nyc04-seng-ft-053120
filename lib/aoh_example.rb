88ee4d7847a2d56acfa2720f8145aa6d3cf1cd5e
Karem Ceron <karemiceron@gmail.com>
2020-06-04 16:49:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,13 +27,6 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  [
-    { name:  "Don Gately", occupation: "Live-in Staff" },
-    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    { name:  "Pat Monteseian", occupation: "Staff" },
-    { name:  "Kate Gompert", occupation: "None" },
-    { name:  "Bruce Green", occupation: "Fan of Mildred" }
-    ]
   aoh[row][column]
 end
 
